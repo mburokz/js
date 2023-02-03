26c22311afcaf41cb3d975af1746f44f8a2d0074
Mburokz <65459470+mburokz@users.noreply.github.com>
2023-02-04 01:16:32 +0700

    Create tv.js

---
+++ tv.js
@@ -0,0 +1,62 @@
+<button onclick="myChannel()">Channel Lainnya</button>
+	<div id="myTV">
+<div class="rspv-tabel">
+<table>
+   <tbody>
+      <tr>
+        <td><a href="/p/trans-tv.html" target="_blank">
+      <img alt="Trans TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEg7TygTBMhC_RwbsFEHIhLVAVfJUIdq1bAFWjzwg_6U_u6VcL1VKNM84xK7b6GtE2ZdcES7Zx4-VwX1eMncTXZ4fjmPqQL6EA2k4QR22QfSuPkQiCwCjKKjPVhKQphjJ-Sk4VbP0yuE2s78HB54wNgn7um-E2UCxFnwcCANNlJvIkqPEUHduRHu0625pA"/>
+</a></td>
+        <td><a href="/p/trans-7.html" target="_blank">
+      <img alt="Trans 7 Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEioE2-gamz1iEto5IY61sJCJ9iB32V4vZz0Uolw6BbmbtOcpb95Kp9pyRvLK-7Nv4LMaKmQ_995OLdtDwRkCRc8ydsdkCy_-4oGV1FNQ6vX0VXHEsp8X1dg5W1U1o_pGHYC4Dc5VxjX2TyGlJfOv6F0BRzu9uf3Hm8BAwnVWVRtQIO3aZ0rd29Tg-w3EA"/>
+</a></td>
+        <td><a href="/p/metro-tv.html" target="_blank">
+      <img alt="Metro TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEhFrS52G1fQCip9L9JQ-W8L2Wr9Z1m5h6GK51snMxAArTBD70if7FyOP34yeDk196LGXRKwigui7lluqV49yJX9H0Zm4fNDgsoAbxpVLW3qeoLkjGE-8XoWCpvg0l-Hje_hNY8zoA9W4Ac-GAk8qWoOw_Jka7VpCNqyDNGYaoNq2y3Y-E4lEY3APYtwzw"/>
+</a></td>
+                <td><a href="/p/tvone.html" target="_blank">
+      <img alt="TV One Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEgwJ5g0PGcgIDOm91m-Thk_S28EErZRqvaXiQA4KrWnrGXIqMXMm34qBLKdsdK77S6qFrFkibrB9gL287taj5qE8p7v-sreItNcxjpV3LZRgLvgJo-tvxTRUwogBOyhA2iJH5YkscDt_Pgk2_-z-ZR46XnK4LG64ZHawNo3-wf6DqkOno84N2e05j3Jiw"/>
+</a></td>
+      </tr>
+      <tr>
+        <td><a href="/p/antv.html" target="_blank">
+      <img alt="ANTV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEgUjs6nB5zjY5DyG-0B-ktj5brz8OkXj2iT4Ir3cLEGBdfZc3YjUvQvFqe8hJ52w922mhVUPXG4zWKtSFYD5y7vW7fyv7deEV3grMSBlfwViicKJ5oaRdlp64EhQYF7BzWQlSaLZmOuHzoyWGI7a_2c2c2A51Gokz7rqVTWleW27u2WG2szYUkDDHbWnA"/>
+</a></td>
+        <td><a href="/p/rcti.html" target="_blank">
+      <img alt="RCTI Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEiWAgmF2cPE93J_XwQ1jV048Y7xP9KhihM7UjuwzDfF5Q07sNdj0UA0HEMFJ4vD9bqy3hw1dTzWEYNo0UibVyYcjsXUFrJIT6iCJyFU53gEkh5XFVo3K4hIUHvDymNL8tQZ_vxlyFDvV0Q5Zgl25JIxaon_TsYeslnoLZ9UMH7D_ACEYpz1SJ1k3jV9Qw"/>
+</a></td>
+        <td><a href="/p/mnctv.html" target="_blank">
+      <img alt="MNCTV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEhinh4mzfoQYmu0NZvrPrPeFw69eqPgszrN7RI9ohWllDe76lP8JNbtppxbfbnnhlW7lQLo506ghPvgm1HIUvSpNZhqq2-W6gbL8j8LJ68DSbibYhGRWfA-PcNV7RakCn7WNpiRapqNvwEtIiPZQOTDDHjskbvWjwmkv7b-Oi0p5PMfAlXwAEMkjZjfwQ"/>
+</a></td>
+        <td><a href="/p/gtv.html" target="_blank">
+      <img alt="GTV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEjQC_WoQw5qU7ZHqaQIPozARwD3RzpDxfzyxEM6EPhqCPX8kVlra_J8sqzk3nQ582bPVaMRxJ1u1EjgMec7ZUSRNpvDqboFU4TTVaYCLYSdnFtwHB_wmxvRo6M8xbnxTHCp86RcSzQjZtOiy_Yf2TL53AkSHVPmMcHsNSK14mKBiTxhqkFwP0DQkHWqHg"/>
+</a></td>
+      </tr>
+      <tr>
+        <td><a href="/p/cnn.html" target="_blank">
+      <img alt="CNN Indonesia Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEjmeX6nM85bKvXkgKe3dhk7SBmQo7lrsuKa4jkI23uIPnUM3bOPMWAIO1u_P3kLPtLEjMi9qczm4NrDeMclRF0JFZKWq_-fe-5po6pX_t-dNHh9z45ahC3UIOK4Ym2l5-2FLgf02KPxbbcQJLeZy6R2RhZui4xDxKRppdF3ob5uU8n22_e1cABc3d9aeQ"/>
+</a></td>
+        <td><a href="/p/cnbc.html" target="_blank">
+      <img alt="CNBC Indonesia Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEjpj8i_O5xD-IMUaCnrt262ppmlo4-0UsCcXrFHRsJg30U3VHaW4c5X1UeRsMYtAXJcjsRe-AdsQOr6X4Yfvt5spo7KTwRr1tdcnVcxnY29PlBBHoDK3seqfa05FZxQsTLuw6Nir9v37gLL5B_fMKzDEIMLkpxuAyv-W7q9cwkqE-2U72gWVzdBM8ubUA"/>
+</a></td>
+        <td><a href="/p/net-tv.html" target="_blank">
+      <img alt="NET. TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEhexGmk6q_rFIUCsbGa47N3KIB3ET0iYbtT3-vg8yCiZ0RFyx-RGkLTqosmQ7syfBJzg8ocvREcwmBB_mK4qELrt2INiuHHUq58wgS-bfMS8GDOdUyWKLbod2IIIQaW5iunCUt3muHEMABAuQCtk2t5FMngViiH2z8oivUaFqNfIxACM0uh8J-trNvgLw"/>
+</a></td>
+        <td><a href="/p/kompas-tv.html" target="_blank">
+      <img alt="Kompas TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEhY3tjLBLsINip4jkTF5DQQ_DycdYO0H9EdFTYU37GrUKkfbU9ybpwaNVWFziMF16d6RXvD_otroONFhiAj8FW7EG7orMLDUqyVi8HTF_sDcpKD0AXlnY95-wxXIvaBqVuHigGK_3TlcLFghBTNhBsXRyZf1_OrvfFsQL-NQaHybQWa4MDeAYPdM_lpyw"/>
+</a></td>
+      </tr>
+     <tr>
+               <td><a href="/p/inews-tv.html" target="_blank">
+      <img alt="iNews TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEhtamYIS15J-N_ZLZoS_icH5QQFqQjkD1OYeU_WVPkZlQNpOI9fIK3LfK8v5NUQkXqxTXUNV8d9-5Q4vKTEXIkj9oi2ZRKh-KVMiVhsTCrseASVo0tCwE3JNV56P1jP1wiWGa9ji9jSA3VUU1p7vkJFRNPNMIE7YxWNzwiXarm-b6UXGXx1zp2-NDFaqg"/>
+</a></td>
+       <td><a href="/p/beritasatu-tv.html" target="_blank">
+      <img alt="BeritaSatu TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiNXQ-jYErIEze1CnbPlJMugbTm__wDksSA285a6E9rRQvM0ecHYO0tK3oi2N5z5IXvTuIN2VsRpiWldF2VZF0S1pJqOVTWUt6muShoSlFj1F7f2xYTRHCZ9IjiYQPTwJddCoSYXWYm7t7pyN7H3adKqKog5DtD_n3YhItqrPqtgX27UWUz2dLvPmPgUQ/s16000/BTV.webp"/>
+</a></td>
+                  <td><a href="/p/rasyaad-tv.html" target="_blank">
+      <img alt="Rasyaad TV Live Streaming" class="lazy" border="0" data-original-height="117" data-original-width="300" height="117" src="https://blogger.googleusercontent.com/img/a/AVvXsEg3E7aCelHSQfJWi9yQ2utkiak0PiFRW-dERzQvfdtvKujZLSirrHPwz8ZPcB9WBqaEQS6W00FFa9YLDfxEkQV3dowqnkJRDNGSKrZ7mG8TOQCl9iL_n7lNa8ChYv1diFnJQPpx-pA4OWxrTpniL-Rpyw-3y7UUwjdyTjT6BOmpykH-hhZNdkf7g7JsFA"/>
+</a></td>
+     </tr>
+   </tbody>
+</table>
+</div>
+  </div>
